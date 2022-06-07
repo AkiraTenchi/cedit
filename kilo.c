4da95bb8f425a86a46fa39faf5741dc449128db8
AkiraTenchi <jimjan2002@gmail.com>
2022-06-07 15:09:39 +0200

    fixed escape sequences

--- kilo.c
+++ kilo.c
@@ -1,22 +1,22 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <termios.h>
+#include <unistd.h>
 
 struct termios orig_termios;
 
-void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
-}
+void disableRawMode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); }
 
 void enableRawMode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
 
-  struct termios raw = orig_termios; 
+  struct termios raw = orig_termios;
 
-  raw.c_lflag &= ~(ECHO | ICANON);
-  
+  raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
@@ -24,6 +24,12 @@ int main() {
   enableRawMode();
 
   char c;
-  while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+     printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
   return 0;
 }
